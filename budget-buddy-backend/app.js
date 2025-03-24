97d659dbcd8c3e3dacf6844f73854cf01e4eb4a9
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-03-24 11:07:06 +0530

    Added frontend url in cors

--- budget-buddy-backend/app.js
+++ budget-buddy-backend/app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "https://budget-buddy-frontend-phi.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
